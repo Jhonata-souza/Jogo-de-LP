c39cc9753f0f1fd17244268bbfc9d802baceeb21
claytonalvesSs <clayton.santos63@etec.sp.gov>
2023-09-20 10:39:55 -0300

    Finalização Android Studio

--- app/src/main/java/com/example/purshecat/TelaCenario3.java
+++ app/src/main/java/com/example/purshecat/TelaCenario3.java
@@ -19,9 +19,4 @@ public class TelaCenario3 extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void IrMapa (View view){
-        Uri linkmapa = Uri.parse("https://www.google.com/maps/place/Guilin,+Guangxi,+China/@25.2877098,110.2914047,12z/data=!3m1!4b1!4m6!3m5!1s0x36a4f43675215133:0xff86c1191d704ae1!8m2!3d25.2736099!4d110.29002!16zL20vMDFzOXpw?hl=pt-BR&entry=ttu");
-        Intent it = new Intent(Intent.ACTION_VIEW,linkmapa);
-        startActivity(it);
-    }
 }
